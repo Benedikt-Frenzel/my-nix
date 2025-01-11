9c7f7b4f0a8db4de299c7edeaaa632b850e8235d
Benedikt Frenzel <git@benediktfrenzel.de>
2025-01-11 12:47:30 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Benedikt sein Flöckchen";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... } @ inputs: 
+  let
+	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+  in
+  {
+	nixosConfigurations.dwarf = nixpkgs.lib.nixosSystem {
+		specialArgs = { inherit inputs; };
+		modules = [
+			./configuration.nix
+			
+		];
+	};
+  };
+}
