514ebf86147e82c1b64c9714f06bfeafe6fbbda3
Benedikt Frenzel <git@benediktfrenzel.de>
2025-01-11 13:04:53 +0100

    some cleanup

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Benedikt sein Flöckchen";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
