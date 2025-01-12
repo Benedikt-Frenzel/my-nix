e0881075842460eb1bfb3a7c8c57741c42a72ace
Benedikt Frenzel <git@benediktfrenzel.de>
2025-01-12 12:21:55 +0100

    system: bluetooth

--- systems/dwarf/configuration.nix
+++ systems/dwarf/configuration.nix
@@ -120,6 +120,9 @@
   # networking.firewall.enable = false;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Bluetooth would be nice
+  services.blueman.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
